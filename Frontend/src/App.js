446cc3676f7532a62b6e8a09704bce6ecf40daad
Giacomo Guaresi <guaresi.giacomo@gmail.com>
2025-06-03 17:38:26 +0200

    feat: aggiungere logo SVG e implementare il tema Material UI nella struttura dell'app

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -14,23 +14,41 @@ import ProductionOrderViewPage from './pages/ProductionOrderViewPage';
 
 import BomPage from './pages/BomPage';
 
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+// Definisci il tuo tema qui
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#d72e28',
+      light: '#80e27e',
+      dark: '#087f23',
+      contrastText: '#fff',
+    },
+  },
+});
+
 function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Dashboard />} />
-        <Route path="/inventory" element={<InventoryPage />} />
-        <Route path="/inventory/edit/:id" element={<InventoryEditPage />} />
-        <Route path="/inventory/view/:id" element={<InventoryViewPage />} />
-        <Route path="/inventory/create" element={<InventoryCreatePage />} />
+   return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline /> {/* Per reset degli stili Material UI */}
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Dashboard />} />
+          <Route path="/inventory" element={<InventoryPage />} />
+          <Route path="/inventory/edit/:id" element={<InventoryEditPage />} />
+          <Route path="/inventory/view/:id" element={<InventoryViewPage />} />
+          <Route path="/inventory/create" element={<InventoryCreatePage />} />
 
-        <Route path="/ProductionOrder" element={<ProductionOrderPage />} />
-        <Route path="/ProductionOrder/:id" element={<ProductionOrderViewPage />} />
+          <Route path="/ProductionOrder" element={<ProductionOrderPage />} />
+          <Route path="/ProductionOrder/:id" element={<ProductionOrderViewPage />} />
 
-        <Route path="/BOM" element={<BomPage />} />
-      </Routes>
-    </Router>
+          <Route path="/BOM" element={<BomPage />} />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
--- Frontend/src/components/Navbar.js
+++ Frontend/src/components/Navbar.js
@@ -3,10 +3,13 @@ import React from 'react';
 import { AppBar, Toolbar, Typography, Button } from '@mui/material';
 import { Link } from 'react-router-dom';
 
+import logo from '../logo.svg';
+
 const Navbar = () => (
   <AppBar position="static">
     <Toolbar>
-      <Typography variant="h6" sx={{ flexGrow: 1 }}>
+      <img src={logo} alt="Logo" style={{ height: 40 }}/>
+      <Typography variant="h6" sx={{ flexGrow: 1, marginLeft: 1 }}>
         Lite ERP
       </Typography>
       <Button color="inherit" component={Link} to="/">Dashboard</Button>
